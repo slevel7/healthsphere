e4db63305f41e7d2e1fa58aeb170c821e15fb018
slevel7 <154259689+slevel7@users.noreply.github.com>
2025-06-04 02:56:08 +0530

    Fix test import path

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hsadmin/main.dart';
+import 'package:healthsphere_admin/main.dart';
 
 void main() {
   testWidgets('renders login screen on startup', (WidgetTester tester) async {
